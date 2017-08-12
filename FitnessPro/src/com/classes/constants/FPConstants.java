b151a0c2525e973d9542fdce24c1a6645e68c4d9
Jillala Srinivas Reddy <srinivasreddy.j@gmail.com>
2017-08-12 15:38:05 +0530

    Add files via upload

--- FitnessPro/src/com/classes/constants/FPConstants.java
+++ FitnessPro/src/com/classes/constants/FPConstants.java
@@ -13,4 +13,24 @@ public class FPConstants {
 	
 	public static final String REGISTRATION_FORM = "/doRegister";
 	
+	public static final String GOALS = "goals";
+	
+	public static final String ADD_GOAL = "usergoalsetting";
+	
+	public static final String USER_ID = "userID";
+	
+	public static final String SAVE_GOAL = "saveUserGoals";
+	
+	public static final String NO_OF_STEPS = "noofsteps";
+	
+	public static final String PUSHUPS = "pushups";
+	
+	public static final String CYCLING = "cyling";
+	
+	public static final String ABS = "abs";
+	
+	public static final String BRISKWALK = "briskwalk";
+	
+	public static final String GOALS_SERVICE = "goalsServiceProvider";
+	
 }
--- FitnessPro/src/com/classes/constants/JSPConstants.java
+++ FitnessPro/src/com/classes/constants/JSPConstants.java
@@ -3,4 +3,9 @@ package com.classes.constants;
 public class JSPConstants {
 
 	public static final String REGISTRATION_HOME = "registration";
+	
+	public static final String ADD_GOAL = "usergoalsetting";
+	
+	public static final String HOME = "home";
+	
 }

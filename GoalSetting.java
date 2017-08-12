fc968588e6bfba1249e8101a9339af3edb77d71d
Jillala Srinivas Reddy <srinivasreddy.j@gmail.com>
2017-08-12 14:35:21 +0530

    Add files via upload
    Goalsettings pojo java file

---
+++ GoalSetting.java
@@ -0,0 +1,62 @@
+
+public class GoalSetting {
+	
+	private int steps ;
+	private int pushups ;
+	private int cycling ;
+	private int abs;
+	private int squats ;
+	
+	public GoalSetting(int steps,int pushups,int cycling,int abs,int squats) 
+	{
+		this.steps=steps ;
+		this.pushups=pushups ;
+		this.cycling=cycling;
+		this.abs=abs;
+		this.squats=squats ;
+	}
+
+	public int getSteps() {
+		return steps;
+	}
+
+	public void setSteps(int steps) {
+		this.steps = steps;
+	}
+
+	public int getPushups() {
+		return pushups;
+	}
+
+	public void setPushups(int pushups) {
+		this.pushups = pushups;
+	}
+
+	public int getCycling() {
+		return cycling;
+	}
+
+	public void setCycling(int cycling) {
+		this.cycling = cycling;
+	}
+
+	public int getAbs() {
+		return abs;
+	}
+
+	public void setAbs(int abs) {
+		this.abs = abs;
+	}
+
+	public int getSquats() {
+		return squats;
+	}
+
+	public void setSquats(int squats) {
+		this.squats = squats;
+	}
+
+	
+	
+	
+}

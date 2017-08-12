6e1897fd99acfaad087550de19b0bbe5322d9ca8
Jillala Srinivas Reddy <srinivasreddy.j@gmail.com>
2017-08-12 15:39:36 +0530

    Add files via upload

---
+++ FitnessPro/src/com/classes/dao/provider/GoalsDAOProvider.java
@@ -0,0 +1,13 @@
+package com.classes.dao.provider;
+
+import com.classes.pojo.GoalSetting;
+
+public class GoalsDAOProvider {
+
+	public boolean addGoal(GoalSetting goal) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
+}
